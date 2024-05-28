50fd8e601b274c8f571c29a173923086d84a1418
NoahMil <noah.miliani13@gmail.com>
2024-05-28 12:05:49 +0200

    Add SFX/Music Manager

--- Assets/Noah/Scripts/ThistleController.cs
+++ Assets/Noah/Scripts/ThistleController.cs
@@ -5,7 +5,6 @@ namespace Noah.Scripts
 {
     public class ThistleController : FlowerController
     {
-        private Vector2 _moveShield;
         [SerializeField] private GameObject shield;
         [SerializeField] private float shieldDistance = 1.0f;  // Distance from player to shield
 
@@ -14,25 +13,6 @@ namespace Noah.Scripts
             shield.SetActive(true);
         }
 
-        private void OnShieldJoystick(InputAction.CallbackContext context)
-        {
-            _moveShield = context.ReadValue<Vector2>();
-        }
-
-        private void Update()
-        {
-            ShieldJoystick();
-        }
-
-        private void ShieldJoystick()
-        {
-            if (_moveShield != Vector2.zero)
-            {
-                Vector3 shieldDirection = new Vector3(_moveShield.x, 0, _moveShield.y).normalized; 
-                Vector3 shieldPosition = transform.position + shieldDirection * shieldDistance;
-                shield.transform.position = shieldPosition;
-            }
-        }
 
         protected override void PassiveCapacity()
         {
